cd1caaa064a65c30e10ea5a45cd62a99ff837d56
frediflexta <fredrick.adegoke@gmail.com>
2018-08-15 16:24:15 +0100

    model mock-db use an array of objects to structure database of questions and answers write tests for them, and also integrate travis and coveralls [#159792716]

---
+++ server/models/answers.js
@@ -0,0 +1,46 @@
+// this stores the answers to questions
+
+const answers = [
+  {
+    id: 1,
+    quesID: 5,
+    text: 'A parent is a table that stores the primary key, a child is any table that references the parent with a foreign key',
+  },
+  {
+    id: 2,
+    quesID: 1,
+    text: 'I prefer working with css grid, reasoning about my layout in rows and columns makes it easier',
+  },
+  {
+    id: 3,
+    quesID: 1,
+    text: 'flexbox is the best!',
+  },
+  {
+    id: 4,
+    quesID: 3,
+    text: 'the chicken came first of course!',
+  },
+  {
+    id: 5,
+    quesID: 3,
+    text: 'the chicken had to have come out of an egg, duh!!',
+  },
+  {
+    id: 6,
+    quesID: 4,
+    text: 'Java is an OOP programming language while Java Script is an OOP scripting language',
+  },
+  {
+    id: 7,
+    quesID: 4,
+    text: 'Java creates applications that run in a virtual machine or browser while JavaScript code is run on a browser only',
+  },
+  {
+    id: 8,
+    quesID: 3,
+    text: 'It evolved from a single cell to a full chicken first, before it laid an egg',
+  },
+];
+
+export default answers;
---
+++ server/test/models/model.js
@@ -0,0 +1,34 @@
+import chai from 'chai';
+import questions from '../../models/questions';
+import answers from '../../models/answers';
+
+chai.should();
+
+describe('Questions mock-models', () => {
+  it('should be an array of objects', () => {
+    questions.should.be.an('array');
+  })
+
+  it('Should contain a question string and an id', () => {
+    const ques = [{
+      id: 1,
+      text: 'What is the best way of laying out pages when working with html and css',    
+    }]
+    questions[0].should.deep.equal(ques[0]);
+  })
+})
+
+describe('Answers mock-models', () => {
+  it('should be an array of objects', () =>{
+    answers.should.be.an('array');
+  })
+
+  it('Should contain an answer string and an id', () => {
+    const ans = [{
+      id: 1,
+    quesID: 5,
+    text: 'A parent is a table that stores the primary key, a child is any table that references the parent with a foreign key',
+    }]
+    answers[0].should.be.deep.equal(ans[0]);
+  })
+})
