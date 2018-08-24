6dcf9e64604a0d0338c972849b597591b9d9a4b5
frediflexta <fredrick.adegoke@gmail.com>
2018-08-24 15:39:04 +0100

    feat(auth users): give limited access to unregistered users
    verify header x-token
    
    [#159993708]
    `

---
+++ server/middleware/auth.js
@@ -0,0 +1,34 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const secret = process.env.SECRET;
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header['x-access-token'];
+    if (!token) {
+      return res.status(403).json({
+        success: 'false',
+        message: 'Please sign in',
+      });
+    }
+
+    return jwt.verify(token, secret, (error, decoded) => {
+      if (error) {
+        return res.status(500).json({
+          success: 'false',
+          message: 'system failure',
+        });
+      }
+
+      req.decoded = decoded;
+      return next();
+    });
+  } catch (e) {
+    throw e.message;
+  }
+};
+
+export default auth;
