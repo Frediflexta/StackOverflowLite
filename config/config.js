effba862bc2d1c7f817954e852aaede6823e3aba
frediflexta <fredrick.adegoke@gmail.com>
2018-08-29 03:43:21 +0100

    chore(heroku): host heroku
    reconfig file to enable heroku database intigration
    
    [#160120906]

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = process.env.HEROKU_POSTGRESQL_RED_URL || process.env.DATABASE;
+const connection = process.env.DATABASE_URL;
 
 const pool = (process.env.NODE_ENV === 'test') ? new Pool({ connectionString: connection }) : new Pool({ connectionString: connection }) ;
 
