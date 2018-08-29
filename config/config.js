126c322b4cc32c1faadb5e5a64f93def2181789d
frediflexta <fredrick.adegoke@gmail.com>
2018-08-30 00:17:44 +0100

    chore(host on heroku): integrate database with the app hosted on heroku
    set environment variable
    refactor config file to be readable
    refactor a message output
    
    [#160120906]

--- config/config.js
+++ config/config.js
@@ -3,20 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const connection = process.env.DATABASE_URL || process.env.HEROKU_POSTGRESQL_RED_URL;
+const connection = process.env.DATABASE_URL;
+const productionsConnection = process.env.HEROKU_POSTGRESQL_RED_URL;
 
-// let exportpool;
-// if (process.env.NODE_ENV === 'test') {
-//   exportpool = new Pool({ connectionString: connection });
-// } else if (process.env.NODE_ENV === 'development') {
-//   exportpool = new Pool({ connectionString: connection });
-// } else if (process.env.HEROKU_POSTGRESQL_RED_URL) {
-//   exportpool = new Pool({ connectionString: process.env.HEROKU_POSTGRESQL_RED_URL });
-// }
-// const pool = exportpool;
+let exportPool;
 
-const pool = new Pool({
-  connectionString: connection,
-});
+if (process.env.NODE_ENV === 'test') {
+  console.log(process.env.DATABASE_URL)
+  exportPool = new Pool({ connectionString: connection });
+} else if (process.env.NODE_ENV === 'development') {
+  console.log(process.env.DATABASE_URL)
+  exportPool = new Pool({ connectionString: connection });
+} else if (process.env.NODE_ENV === 'production') {
+  exportPool = new Pool({ connectionString: productionsConnection });
+}
+const pool = exportPool;
 
 export default pool;
