d2631aa88738489c268b5600c684aa9784775a51
frediflexta <fredrick.adegoke@gmail.com>
2018-09-28 18:24:27 +0100

    feat(get all questions): consume api to get all questions
    - fetch all questions from the database
    - refactor UI to suit data from the database
    - refactor query to include number of answers a question has
    
    [Finishes #160825068]

---
+++ UI/client/getQuestions.js
@@ -0,0 +1,50 @@
+const contentHeader = document.querySelector('.h-content');
+const url = '/api/v1/questions';
+
+const loadQuestions = () => {
+  fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => res.json())
+    .then((res) => {
+      if (res.status === 'success') {
+        let htmlContent = '';
+        const questions = res.data;
+
+        if (questions.length > 1) {
+          questions.forEach((question) => {
+            const questionUrl = `/api/v1/questions/${question.id}`;
+            htmlContent += `<div class="content-container">
+              <div class="votes">
+                <div class="v-counts">
+                  <span title="">2</span>
+                </div>
+                <div class="quote">votes</div>
+            </div>
+            <div class="answers">
+              <div class="ans-counts">
+                <span>${question.totalanswers}</span>
+              </div>
+              <div class="quote">answers</div>
+            </div>
+            <div class="ques-details">
+              <h3><a href="${questionUrl}">${question.questitle}</a></h3>
+              <p>${question.quesbody}</p>
+            </div>
+          </div>`;
+          });
+          contentHeader.insertAdjacentHTML('afterend', htmlContent);
+        }
+      } else if (res.status === 'fail') {
+        throw new Error(`${res.message}`);
+      }
+    }).catch((error) => {
+      let htmlContent = '';
+      htmlContent = `<div class="error-no-image">Sorry! ${error.message} (${error.error})</div>`;
+      contentHeader.insertAdjacentHTML('afterend', htmlContent);
+    });
+};
+
+window.onload = loadQuestions();
--- server/models/queries.js
+++ server/models/queries.js
@@ -1,8 +1,8 @@
 const queries = {
   signUp: 'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING *',
   signIn: 'SELECT * FROM users WHERE username = $1',
-  getAllQuetions: 'SELECT * FROM questions',
-  getQuestion: 'SELECT * FROM questions WHERE id = $1',
+  getAllQuetions: 'SELECT questions.id, questions.questitle, questions.quesbody, questions.created_at, questions.userid, COUNT(answers.quesid) AS totalAnswers FROM questions LEFT OUTER JOIN answers ON questions.id=answers.quesid GROUP BY questions.id',
+  getQuestion: 'SELECT questions.id, questions.questitle, questions.quesbody, questions.created_at, questions.userid, users.username FROM questions JOIN users ON questions.userid = users.id WHERE questions.id = $1',
   getAllAnswers: 'SELECT * FROM answers WHERE quesid = $1',
   postQuestion: 'INSERT INTO questions (userid, questitle, quesbody) VALUES ($1, $2, $3)',
   deleteQuestion: 'DELETE FROM questions WHERE id = $1 AND userid= $2',
